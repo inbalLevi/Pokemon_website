ba6f3120686d381b851f88efa20b0fd7fadf0f5a
inbal levi <inbalevi2014@gmail.com>
2020-10-11 12:15:41 +0300

    Updates

---
+++ precache-manifest.0fc61e7d900907956488ea344cb8efea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b9024fc70bc332c6fe5c9b09ddfcae",
+    "url": "/Pokemon_website/index.html"
+  },
+  {
+    "revision": "34ed4eb50efbbb2e259e",
+    "url": "/Pokemon_website/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7cc32a99010b6d6df990",
+    "url": "/Pokemon_website/static/css/main.6f51db7d.chunk.css"
+  },
+  {
+    "revision": "34ed4eb50efbbb2e259e",
+    "url": "/Pokemon_website/static/js/2.e006f4b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokemon_website/static/js/2.e006f4b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cc32a99010b6d6df990",
+    "url": "/Pokemon_website/static/js/main.a0cf5d63.chunk.js"
+  },
+  {
+    "revision": "56091497cf8a9cb40983",
+    "url": "/Pokemon_website/static/js/runtime-main.ae9ee820.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Pokemon_website/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "f76bc1edc5ea0fc216194a67d17536ae",
+    "url": "/Pokemon_website/static/media/poki.f76bc1ed.png"
+  }
+]);
